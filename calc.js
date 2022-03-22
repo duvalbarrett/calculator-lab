f33bf38ccb896c68dea3e4d4fa07ec61c037b688
dstylz <duvalbarrett@gmail.com>
2022-03-22 16:49:33 -0400

    Addition of the statement

--- calc.js
+++ calc.js
@@ -14,14 +14,14 @@ reader.question("What would you like to calculate?", function(input){
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
+	if (mathSymbol === "+"){
+		console.log(num1 + num2);
+	}
+
 
 	// This line closes the connection to the command line interface.
 	reader.close()
 
 });
 
-console.log(`math Symbol`, mathSymbol);
-console.log(`num1`, num1);
-console.log(`num2`, num2);
 
-reader.close()
